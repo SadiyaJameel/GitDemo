f89f55bcb75b0765a4500e07527c9b26613657bd
sadiya <syedasadiyajameel09@gmail.com>
2026-05-07 23:38:50 +0530

    Euro architect commit

--- src/test1.java
+++ src/test1.java
@@ -3,11 +3,10 @@ public class App {
         Calculator c = new Calculator();
 
         System.out.println("2 + 3 = " + c.add(2, 3));
-        System.out.println("new update")
-        System.out.println("another new update")
-        System.out.println(" update from architect")
-        
-        
+        System.out.println("new update");
+        System.out.println("another new update");
+        System.out.println(" update from architect from ASIA");
+        System.out.println(" update from architect from EURO");
         System.out.println("Reverse of hello = " + StringUtils.reverse("hello"));
     }
 }
