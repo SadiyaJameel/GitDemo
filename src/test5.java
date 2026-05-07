c02586d7da1ac34129e48c5282821d11a89ec0f3
sadiya <syedasadiyajameel09@gmail.com>
2026-05-07 23:11:58 +0530

    commit to develop branch

--- src/test5.java
+++ src/test5.java
@@ -1,13 +1,18 @@
-public class Calculator {
+ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
 
     public int subtract(int a, int b) {
         return a - b;
+        return -a + b;
     }
 
     public int multiply(int a, int b) {
+        return a * b;
+        
+    }
+    public int multi(int a, int b) {
         return a * b;
         
     }
